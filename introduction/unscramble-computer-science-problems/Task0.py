c712d385b347e4fb021e0a172e0674698ca34786
Neidson Souza <neidson.ds.souza@gmail.com>
2024-11-11 17:19:46 -0300

    Prints text oldest record

--- introduction/unscramble-computer-science-problems/Task0.py
+++ introduction/unscramble-computer-science-problems/Task0.py
@@ -20,3 +20,8 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+from datetime import datetime
+
+text_ordered_list = sorted(texts, key=lambda x: datetime.strptime(x[2], '%d-%m-%Y %H:%M:%S'))[0]
+text_final_string = f'First record of texts, {text_ordered_list[0]} texts {text_ordered_list[1]} at time {text_ordered_list[2]}'
+print(text_final_string)
