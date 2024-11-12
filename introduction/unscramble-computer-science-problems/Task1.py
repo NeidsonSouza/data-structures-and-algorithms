8157ae65b7f84ca99f1d444000e8f33ece6c0ee8
Neidson Souza <neidson.ds.souza@gmail.com>
2024-11-11 22:10:29 -0300

    Prints the amount of uniq phone numbers

--- introduction/unscramble-computer-science-problems/Task1.py
+++ introduction/unscramble-computer-science-problems/Task1.py
@@ -18,3 +18,22 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+def add_to_list(item, ls):
+    if item not in ls:
+        ls.append(item)
+
+
+def get_uniq_numbers(records, uniqs):
+    for item in records:
+        add_to_list(item[0], uniqs)
+        add_to_list(item[1], uniqs)
+
+    return uniqs
+
+if __name__ == "__main__":
+    uniqs = get_uniq_numbers(texts, [])
+    uniqs = get_uniq_numbers(calls, uniqs)
+
+    sentence = f'There are {len(uniqs)} different telephone numbers in the records.'
+    print(sentence)
