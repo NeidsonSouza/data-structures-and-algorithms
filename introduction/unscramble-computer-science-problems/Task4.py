6afdbb6250aa0f01f8856e1fca220ee2cbac7610
Neidson Souza <neidson.ds.souza@gmail.com>
2024-11-12 15:50:33 -0300

    Prints all caller numbers that could be marketers

--- introduction/unscramble-computer-science-problems/Task4.py
+++ introduction/unscramble-computer-science-problems/Task4.py
@@ -25,3 +25,35 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+def is_number_in_texts(n):
+    for i in texts:
+        if n == i[0]:
+            return True
+        if n == i[1]:
+            return True
+    return False
+
+def is_number_a_receiver(n):
+    for i in calls:
+        if n == i[1]:
+            return True
+    return False
+
+def add_to_list(item, ls):
+    if item not in ls:
+        ls.append(item)
+
+
+def get_uniq_numbers(records, uniqs):
+    for item in records:
+        if item[0].startswith('140'):
+            if not is_number_in_texts(item[0]):
+                if not is_number_a_receiver(item[0]):
+                    add_to_list(item[0], uniqs)
+
+    return uniqs
+
+if __name__ == "__main__":
+    uniqs = get_uniq_numbers(calls, [])
+    print(f'These numbers could be telemarketers:')
+    print(*sorted(uniqs),sep='\n')
