e768cda6b9dc18fc9962530a86e757be55e16837
Neidson Souza <neidson.ds.souza@gmail.com>
2024-11-19 08:16:12 -0300

    Refactor task-0

--- introduction/unscramble-computer-science-problems/Task0.py
+++ introduction/unscramble-computer-science-problems/Task0.py
@@ -31,10 +31,7 @@ def get_ordered_list_by_date(out_of_order_list):
         )
 
 
-def main():
-    first_text_record = get_ordered_list_by_date(texts)[0]
-    last_call_record  = get_ordered_list_by_date(calls)[-1]
-
+def print_sentences(first_text_record, last_call_record):
     print(
         'First record of texts, {0} texts {1} at time {2}'
         .format(*first_text_record)
@@ -43,6 +40,13 @@ def main():
         'Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds'
         .format(*last_call_record)
         )
+
+
+
+def main():
+    first_text_record = get_ordered_list_by_date(texts)[0]
+    last_call_record  = get_ordered_list_by_date(calls)[-1]
+    print_sentences(first_text_record, last_call_record)
 
 
 if __name__ == '__main__':
